d0c26b7717c1567ea0a0dd216dfe2e6976320fc0
DawidKahla <dawid.kahla@onet.pl>
2024-07-29 22:02:07 +0200

    set up django and PostgreSQL

--- project_manager/projects/models.py
+++ project_manager/projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
